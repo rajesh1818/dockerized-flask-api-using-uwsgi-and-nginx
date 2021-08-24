7cea75feb267deacd6e3032d1878477dcd09586f
rajesh jethwa <58974729+rajesh1818@users.noreply.github.com>
2021-08-24 18:36:17 +0530

    Add files via upload

---
+++ myproject/wsgi.py
@@ -0,0 +1,19 @@
+# import ptvsd
+# ptvsd.enable_attach(
+#     "my_secret",
+#     address=('0.0.0.0',5000)
+# )
+
+
+from master import app
+from user.view import api_route
+from role.view import api_role
+from master.view import api_view
+
+app.register_blueprint(api_route)
+app.register_blueprint(api_view)
+app.register_blueprint(api_role)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
